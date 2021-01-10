42a609ccbf97cb902c7b55569e5d6d57aa25c078
leopeli <leopeli.it@gmail.com>
2021-01-10 19:59:04 -0300

    criado os componentes da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    ProgressoComponent,
+    TentativasComponent
   ],
   imports: [
     BrowserModule,
